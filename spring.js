3764d093817c1f6322094811daf4f44bee6d887a
sugata <k.sugata.sys24@morijyobi.ac.jp>
2025-02-20 22:33:03 +0900

    2025-02-20の成果

--- spring.js
+++ spring.js
@@ -13,10 +13,11 @@ let miss = 0;
 const theme = document.getElementById("theme");
 let theme_index = 0;
 let choose_theme;
+let countStarted = false;
 const theme_list = [["island", "rabbit", "orange", "in", "china", "streamer", "off", "on", "radio", "wave"], 
                     ["control", "monitor", "set", "next", "audio", "by", "scan", "ask", "solve", "core"]];
-function choice() {
-    const startpopupoverlay = document.getElementById("startpopupOverlay");
+                    function choice() {
+                        const startpopupoverlay = document.getElementById("startpopupOverlay");
     startpopupoverlay.style.display = "none";
     presspopupoverlay.style.display = "flex";
     document.addEventListener("keydown", handlekeydown);
@@ -24,6 +25,7 @@ function choice() {
 function handlekeydown(event) {
     if (!isGameStarted && event.key === " ") {
         isGameStarted = true;
+        document.removeEventListener("keydown", handlekeydown);
         presspopupoverlay.style.display = "none";
         document.getElementById("countpopupOverlay").style.display = "flex";
         count()
@@ -31,25 +33,30 @@ function handlekeydown(event) {
 }
 const countdown = document.getElementById("count");
 function count() {
-    countInterval = setInterval(function() {
-        if (countSeconds === 0) {
-            document.getElementById("countpopupOverlay").style.display = "none";
-            document.getElementById("gamepopupOverlay").style.display = "flex";
-            choose_topic()
-        }
-        else if (countSeconds <= 1) {
-            countdown.textContent = "Start";
-            countSeconds--;
-        }
-        else{
-            countSeconds--;
-            countdown.textContent = countSeconds;
-        }
-    }, 1000)
+    if (!countStarted) {
+        countInterval = setInterval(function() {
+            if (countSeconds === 0) {
+                document.getElementById("countpopupOverlay").style.display = "none";
+                document.getElementById("gamepopupOverlay").style.display = "flex";
+                countStarted = true;
+            }
+            else if (countSeconds <= 1) {
+                countdown.textContent = "Start";
+                countSeconds--;
+            }
+            else{
+                countSeconds--;
+                countdown.textContent = countSeconds;
+            }
+        }, 1000)
+    }
+    choose_topic()
 }
 function choose_topic() {
+    theme_index = 0;
     let random = Math.floor(Math.random() * 2);
     choose_theme = theme_list[random];
+    // console.log(choose_theme);
     theme.textContent = choose_theme[theme_index];
     typing();
 }
@@ -58,14 +65,14 @@ function typing() {
     // updateThemeDisplay();
     // input.value = "";
     input.addEventListener("input", function() {
-        if (input.value === theme.textContent) {
+        if (input.value === choose_theme[theme_index]) {
             theme_index++;
             if (theme_index < choose_theme.length) {
-                theme.textContent = choose_theme[theme_index];
                 input.value = "";
+                updateThemeDisplay();
+                theme.textContent = choose_theme[theme_index];
                 // theme.textContent = theme_list[theme_index];
             }
-            updateThemeDisplay();
         } else {
             let currentInput = input.value;
             let correctText = theme.textContent.slice(0, currentInput.length);
@@ -74,22 +81,23 @@ function typing() {
                 mistake.textContent = `ミスタイプ数:${miss}`;
                 input.value = input.value.slice(0, -1);
             }
-            // for (let i = 0; i < input.value.length; i++) {
-            //     if (input.value[i] !== theme.textContent[i]) {
-            //         miss++;
-            //         mistake.textContent = `ミスタイプ数:${miss}`;
-            //         input.value = input.value.slice(0, -1);
-            //     }
-            // }
         }
         updateThemeDisplay();
     })
 }
+// function inputFocus() {
+//     setTimeout(function() {
+//         document.getElementById("input_field").focus();
+//         console.log("a")
+//     }, 100);
+// }
 function updateThemeDisplay() {
-    let currentInput = input.value;
-    let correctPart = choose_theme[theme_index].slice(0, currentInput.length);
-    let remainingPart = choose_theme[theme_index].slice(currentInput.length);
-    theme.innerHTML = `<span style="color: green;">${correctPart}</span>${remainingPart}`;
+    if (theme_index < choose_theme.length) {
+        let currentInput = input.value;
+        let correctPart = choose_theme[theme_index].slice(0, currentInput.length);
+        let remainingPart = choose_theme[theme_index].slice(currentInput.length);
+        theme.innerHTML = `<span style="color: green;">${correctPart}</span>${remainingPart}`;
+    }
 }
 easy.addEventListener("click", choice);
 normal.addEventListener("click", choice);
